417f9276caae8cb614df32ff0501c6cf51c2c351
Giovanni Zago <giovanni.zago0@gmail.com>
2024-10-14 12:14:57 +0200

    end of morning scripting

--- data-processing/puppi_inspector.py
+++ data-processing/puppi_inspector.py
@@ -1,5 +1,7 @@
 from puppi_unpacker_native64 import puppi_unpacker_native64 as unpacker
 
+DATA_PATH = "/home/giovanni/pod/thesis/code/scripts-sources/W3Pi-selection-algorithm/data/"
+
 def print_table(data, headers, start=0, end=10):
     columns = len(headers)
     col_widths = [max(len(str(row[i])) for row in data) for i in range(columns)]
@@ -8,17 +10,17 @@ def print_table(data, headers, start=0, end=10):
 
     print(format_str.format(*headers))
     print('-' * (sum(col_widths) + 3 * (columns - 1)))
-    for row in data[start:end]:
+    for row in data[start:end+1]:
         print(format_str.format(*row))
 
 def main():
-    header_data, part_data = unpacker("./data/Puppi.dump")
+    header_data, part_data = unpacker(DATA_PATH + "Puppi.dump")
     columns_head = ["start_idx", "vld_header", "err_bit", "lr_number", "orbit_cnt", "bx_cnt", "n_cand"]
     columns_part = ["start_idx", "pdg_id", "phi", "eta", "pt"]
 
     print_table(header_data, columns_head)
     print("\n\n\n")
-    print_table(part_data, columns_part, start=44, end=50)
+    print_table(part_data, columns_part, start=0, end=49)
 
 if __name__ == "__main__":
     main()
